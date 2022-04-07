4b3ac239219eb030e9b1257f10e74d2697d49f41
Mohamed Ashraf <mhamedashraf32@gmail.com>
2022-04-07 17:52:12 +0200

    update the web site

--- js/main.js
+++ js/main.js
@@ -1,14 +1,53 @@
 
+let btnTop = document.querySelector(".btn-top");
+
+window.onscroll = function(){
+ this.scrollY >= 600 ? btnTop.classList.add('show'):btnTop.classList.remove('show');
+}
+
+btnTop.addEventListener("click", function(){
+  window.scrollTo({
+    top:0,
+    behavior:"smooth",
+  });
+});
+
+  $(function() {
+    $('.skitter-large').skitter(
+      
+    );
+  });
+
+
   $(document).ready(function(){
     $('.our-partner-container').slick({
-        slidesToShow: 5,
+        slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 1500,
         centerMode : true,
         dots : true,
         arrows : false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 4,
+          
+            }
+          },
+          {
+            breakpoint: 520,
+            settings: {
+              slidesToShow: 3,
+             dots:false,
+            }
+          },
+         
+        ]
+      
     });
   });
 
   new WOW().init();
+  // start skitter
